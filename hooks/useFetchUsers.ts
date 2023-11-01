587d9e15005e66090f3dc7df4375a618fc2d35fe
saifeliyas <saifeliyas@gmail.com>
2023-11-02 02:20:09 +0530

    Un wanted files removed

--- hooks/useFetchUsers.ts
+++ hooks/useFetchUsers.ts
@@ -1,10 +1,10 @@
 import _ from "lodash";
 import { User } from '@/common/models';
 import useSWR from 'swr';
-import fetcher from '@/libs/fetcher';
+import axios from "axios";
 
 const useFetchUsers = () => {
-  let {data, error, isLoading} = useSWR(`/api/data`, fetcher);
+  let {data, error, isLoading} = useSWR(`/api/data`, (url: string) => axios.get(url).then((res) => res.data));
   const users = data ? Object.values(data.userIdMap) : []; 
   return {
     data: _.sortBy(users, "sortOrder") as User[],
--- hooks/useUsers.ts
+++ hooks/useUsers.ts
@@ -1,13 +1,13 @@
 import _ from "lodash";
 import { User } from '@/common/models';
 import useSWR from 'swr';
-import fetcher from '@/libs/fetcher';
+import axios from "axios";
 
 const useUsers = (userIdMap?: Record<string, User>) => {
   let isLoading = true;
   let userMap = userIdMap;
   if(!userIdMap) {
-    let response: any = useSWR(`/api/data`, fetcher);
+    let response: any = useSWR(`/api/data`, (url: string) => axios.get(url).then((res) => res.data));
     userMap = response.userIdMap;
   }
   const data: User[] = userMap ? Object.values(userMap) : [];
